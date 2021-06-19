cf5902ebb02e9c9ce28851696ad1c16010bc0170
JayantGoel001 <jgoel92@gmail.com>
2021-06-19 08:17:38 +0530

    Initial Commit

---
+++ Welcome to AtCoder.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define int long long int
+#define INF 1e18
+using namespace std;
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t=1;
+    while(t--){
+        int a;
+        cin>>a;
+        int b,c;
+        cin>>b>>c;
+        string str;
+        cin>>str;
+        cout<<a+b+c<<" "<<str<<"\n";
+    }
+}
