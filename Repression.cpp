a10c5907bce861c7bb86ae00a271597779ec38b2
Jayant Goel <jgoel92@gmail.com>
2021-06-26 18:06:16 +0530

    Initial Commit

---
+++ Repression.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+#define int long long int
+#define INF 1e18
+using namespace __gnu_pbds;
+using namespace std;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int power(int x, int y, int MOD = INF) {
+    if (y == 0) {
+        return 1;
+    }
+    if (y % 2 == 0) {
+        return power((x * x) % MOD, y / 2) % MOD;
+    } else {
+        return (x * power((x * x) % MOD, (y - 1) / 2) % MOD) % MOD;
+    }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(NULL);
+    int t = 1;
+//    cin >> t;
+//    int MOD = power(10, 9) + 7;
+
+    while (t--) {
+        int ar[3];
+        for (int i = 0; i < 3; ++i) {
+            cin>>ar[i];
+        }
+        sort(ar,ar+3);
+        cout<<ar[1]+ar[2]<<"\n";
+    }
+}
