f8a913f5d8a0a24b68b875c392bb18cd8bcba27d
JayantGoel001 <jgoel92@gmail.com>
2021-07-04 18:15:21 +0530

    Initial Commit

--- 3.cpp
+++ 3.cpp
@@ -2,7 +2,7 @@
 #include<ext/pb_ds/assoc_container.hpp>
 #include<ext/pb_ds/tree_policy.hpp>
 
-#define int long long int
+#define int unsigned long long int
 #define double double_t
 #define INF 1e18
 using namespace __gnu_pbds;
@@ -27,17 +27,41 @@ int power(int x, int y, int MOD = INF) {
         return (x * power((x * x) % MOD, (y - 1) / 2) % MOD) % MOD;
     }
 }
-
+class Pair{
+public:
+    int indices;
+    int id;
+    int pieces;
+};
+bool sortByValue(Pair p1,Pair p2){
+    return p1.id<=p2.id;
+}
+bool sortByIndices(Pair p1,Pair p2){
+    return p1.indices < p2.indices;
+}
 int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
     int t = 1;
-    cin >> t;
     while (t--) {
-        int n;
-        cin >> n;
-
-        cout << "\n";
+        int n,k;
+        cin >> n>>k;
+        int miniValue = k/n;
+        int remaining = k%n;
+        Pair ar[n];
+        for (int i = 0; i < n; ++i) {
+            cin>>ar[i].id;
+            ar[i].indices = i;
+            ar[i].pieces = miniValue;
+        }
+        sort(ar,ar+n,sortByValue);
+        for (int i = 0; i < remaining; ++i) {
+            ar[i].pieces+=1;
+        }
+        sort(ar,ar+n, sortByIndices);
+        for (int i = 0; i < n; ++i) {
+            cout<<ar[i].pieces<<"\n";
+        }
     }
 }
