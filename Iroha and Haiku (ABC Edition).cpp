cb6cfb634a0a5d5794d798b02a93dc08bedcd037
JayantGoel001 <jgoel92@gmail.com>
2021-07-12 10:02:15 +0530

    Initial Commit

---
+++ Iroha and Haiku (ABC Edition).cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+#define int long long int
+#define double double_t
+#define INF 1e18
+using namespace __gnu_pbds;
+using namespace std;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int GCD(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return GCD(b, a % b);
+}
+
+int power(int x, int y, int MOD = INF) {
+    if (y == 0) {
+        return 1;
+    }
+    if (y % 2 == 0) {
+        return power((x * x) % MOD, y / 2) % MOD;
+    } else {
+        return (x * power((x * x) % MOD, (y - 1) / 2) % MOD) % MOD;
+    }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t = 1;
+    while (t--) {
+        int ar[3];
+        for (long long & i : ar) {
+            cin>>i;
+        }
+        sort(ar,ar+3);
+        if (ar[0]==5 && ar[1] ==5 && ar[2]==7){
+            cout<<"YES";
+        } else{
+            cout<<"NO";
+        }
+        cout << "\n";
+    }
+}
