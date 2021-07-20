942008ce13efa2b6715bf1d4bd4c32fadeaae328
JayantGoel001 <jgoel92@gmail.com>
2021-07-20 12:28:46 +0530

    Initial Commit

---
+++ Increasing Triples.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+#define int long long int
+#define double double_t
+#define INF 1e18
+using namespace __gnu_pbds;
+using namespace std;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int GCD(int a, int b) {
+    return b == 0 ? a : GCD(b, a % b);
+}
+
+int power(int x, int y, int MOD = INF) {
+    if (y == 0) {
+        return 1;
+    }
+    if (y % 2 == 0) {
+        return power((x * x) % MOD, y / 2) % MOD;
+    } else {
+        return (x * power((x * x) % MOD, (y - 1) / 2) % MOD) % MOD;
+    }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t = 1;
+    while (t--) {
+        int n;
+        cin >> n;
+        int ar[n],br[n],cr[n];
+        for (int i = 0; i < n; ++i) {
+            cin>>ar[i];
+        }
+        for (int i = 0; i < n; ++i) {
+            cin>>br[i];
+        }
+        for (int i = 0; i < n; ++i) {
+            cin>>cr[i];
+        }
+        sort(ar,ar+n);
+        sort(br,br+n);
+        sort(cr,cr+n);
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int ans = 0;
+        while (i<n){
+            while (j<n && br[j]<=ar[i]){
+                j++;
+            }
+            if (j>=n){
+                break;
+            }
+            while (k<n && cr[k]<=br[j]){
+                k++;
+            }
+            if (k>=n){
+                break;
+            }
+            ans++;
+            i++;
+        }
+        cout<<ans;
+        cout << "\n";
+    }
+}
