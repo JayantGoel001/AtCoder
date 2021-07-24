3256b40b8fa99a179f0ea878ea45b5fd069900ad
JayantGoel001 <jgoel92@gmail.com>
2021-07-24 20:06:41 +0530

    Initial Commit

--- 3.cpp
+++ 3.cpp
@@ -24,16 +24,37 @@ int power(int x, int y, int MOD) {
         return (x * power((x * x) % MOD, (y - 1) / 2, MOD) % MOD) % MOD;
     }
 }
-
+const int MOD = (int)(1e9 + 7);
 int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
     int t = 1;
-    while (t--) {
-        int n;
-        cin >> n;
 
+    while (t--) {
+        string str;
+        cin >> str;
+        string str2 = "chokudai";
+        int **dp = new int*[str.length()+1];
+        for (int i = 0; i <= str.length(); ++i) {
+            dp[i] = new int[str2.length()+1];
+        }
+        for (int i = 0; i <=str.length(); ++i) {
+            for (int j = 0; j <=str2.length(); ++j) {
+                if (j==0){
+                    dp[i][j] = 1;
+                } else if (i==0){
+                    dp[i][j] = 0;
+                }else {
+                    if (str[i - 1] == str2[j - 1]) {
+                        dp[i][j] = (dp[i - 1][j]%MOD + dp[i - 1][j - 1]%MOD) % MOD;
+                    } else {
+                        dp[i][j] = dp[i - 1][j] % MOD;
+                    }
+                }
+            }
+        }
+        cout<<dp[str.length()][str2.length()]%MOD;
         cout << "\n";
     }
 }
